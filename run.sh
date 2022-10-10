35f8cbc182eb29cd4ecfb0145ec3ca423a904f58
Giacomo Benedetti <giacomo.bndt@gmail.com>
2022-10-10 14:56:39 +0200

    fixing run.sh and missing tmp folder

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-./anonymized -sample samples/$1 -time $2 -gh-token <ghtoken>
+./sunset -sample samples/$1 -time $2 -gh-token <ghtoken>
 exps_dir=$(ls -td experiments/* | head -n 1)
 cat $exps_dir/workflow* > $exps_dir/workflow_tot.yml
 python3 wfExtractor.py --wf $exps_dir/workflow_tot.yml --dest $exps_dir
